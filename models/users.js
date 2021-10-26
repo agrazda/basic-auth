6f9b18a65e0356ace4c2c08083776779b84791df
ALEX GRAZDA <AGRAZDA@GMAIL.COM>
2021-10-25 23:35:37 -0700

    server up

---
+++ models/users.js
@@ -0,0 +1,15 @@
+'use strict'
+
+
+// const Users = sequelize.define('User', {
+//     username: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     },
+//     password: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     }
+//   });
+
+//   module.export(U)
